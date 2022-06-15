5c14a84872b831f4c1b43ef28a1e831cd4ccb4ca
Dare54 <88983147+Dare54@users.noreply.github.com>
2022-06-15 02:44:59 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,54 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { Dimensions, StyleSheet, SafeAreaView, Alert, Platform, View} from "react-native";
-import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
+import React from "react";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  
-  return (
-    <SafeAreaView style={styles.container}>
-      <View 
-        style={{
-          backgroundColor: '#fff',
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          alignContent: "center",
-        }}
-      >
-        <View style={{
-            backgroundColor: "dodgerblue",
-            width: 100,
-            height: 100,
-          }}
-        />
-        <View style={{
-            backgroundColor: "gold",
-            width: 100,
-            height: 100,
-            bottom: 20,
-          }}
-        />
-        <View style={{
-            backgroundColor: "tomato",
-            width: 100,
-            height: 100,
-          }}
-        />
-      
-
-      </View>
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
 
-const containerStyle = { backgroundColor: "orange"}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // justifyContent: "center",
-    // alignItems: "center"
-  },
-});
